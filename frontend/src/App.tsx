3dc6ce1b5b6d5f4159f40c42e7525c2ae98c3c67
rondinelli-oliveira <rondinelliads@gmail.com>
2022-07-14 15:01:19 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import NotificationButton from "./components/NotficationButton";
 
 function App() {
-  return(
-    <h1>Hello world!</h1>
-  )
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
